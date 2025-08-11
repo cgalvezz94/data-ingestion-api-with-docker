387a7fd672bd5afdb17aba90e22fc28dc142b5a6
Camilo <camilogalvez10@gmail.com>
2025-08-11 13:54:16 -0400

    [Add]: Config.py: Some paths and batch size var.

---
+++ src/config.py
@@ -0,0 +1,19 @@
+import os
+
+# Where config.py is
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Where CSVs and db are 
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+# Full path to SQLite file
+SQLITE_DB_PATH = os.path.join(DATA_DIR, "database.db")
+
+# CSV paths
+CSV_DEPARTMENTS = os.path.join(DATA_DIR, "departments.csv")
+CSV_HIRED_EMPLOYEES = os.path.join(DATA_DIR, "hired_employees.csv")
+CSV_JOBS = os.path.join(DATA_DIR, "jobs.csv")
+
+
+# Batch insert max rows
+BATCH_SIZE = 1000
