8172977d4682aeb1e29451573fc79ac2b7ed5965
Camilo <camilogalvez10@gmail.com>
2025-08-12 13:55:37 -0400

    [Change]: data paths changed based on pytest use

--- src/config.py
+++ src/config.py
@@ -1,10 +1,16 @@
 import os
 
+# Detectar si estamos ejecutando pytest
+IS_TEST = "PYTEST_CURRENT_TEST" in os.environ
+
 # Where config.py is
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Where CSVs and db are 
-DATA_DIR = os.path.join(BASE_DIR, "data")
+# DATA directory (diferente si es test)
+if IS_TEST:
+    DATA_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "tests", "data"))
+else:
+    DATA_DIR = os.path.join(BASE_DIR, "data")
 
 # Full path to SQLite file
 SQLITE_DB_PATH = os.path.join(DATA_DIR, "database.db")
@@ -16,7 +22,6 @@ SQLALCHEMY_DATABASE_URL = f"sqlite:///{SQLITE_DB_PATH}"
 CSV_DEPARTMENTS = os.path.join(DATA_DIR, "departments.csv")
 CSV_HIRED_EMPLOYEES = os.path.join(DATA_DIR, "hired_employees.csv")
 CSV_JOBS = os.path.join(DATA_DIR, "jobs.csv")
-
 
 # Batch insert max rows
 BATCH_SIZE = 1000
